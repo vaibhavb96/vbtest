03c7126189724a1fde4f057566e0cafb91e4f403
vaibhav <vaibhavvb96@gmail.com>
2021-02-14 12:22:06 +0530

    done

---
+++ test2.py
@@ -0,0 +1,50 @@
+
+
+#
+# for num in range(100,201):
+#     # if num>1:
+#         for i in range(2,num):
+#             if (num%i)==0:
+#                 break
+#         else:
+#             print(num)
+
+
+# a=[12,34,11,45,67,87,65,23,45,67,89,9,455,342,322,54,31,43,44,222,87,54,35,26,17,15]
+# a.sort()
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+# a=['']
+# a.sort()
+
+# without using sort descending order
+# new_a=[]
+# while a:
+#     minimum=a[0]
+#     for x in a:
+#         if x>minimum:
+#             minimum=x
+#     new_a.append(minimum)
+#     a.remove(minimum)
+# print(new_a)
+
+
+# word = str(input("enter a word to check"))
+# def isPalindrome(word):
+#     rev=''.join(reversed(word))
+#     if (word==rev):
+#         return True
+#     return False
+# isPalindrome('madam')ganesh
+
+# word = str(input("enter a word to check"))
+# if word[::-1]==word:
+#     print('palindrome')
+# else:
+#     print('not')
+l=[1,2,3,4,1,5,6,7,4,8,9,3]
+
+
+# print(set([x for x in l if l.count(x)>1]))
+
